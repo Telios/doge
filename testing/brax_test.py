a4c43ab8861a7ceb0c16e4fd311158fb052146a1
Telios <victor.mittermair@gmail.com>
2024-07-04 06:32:23 +0000

    brax tests

---
+++ testing/brax_test.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from etils import epath
+import functools
+from IPython.display import HTML
+from typing import Any, Dict, Sequence, Tuple, Union
+import os
+os.environ['MUJOCO_GL'] = 'egl'
+from ml_collections import config_dict
+
+import jax
+from jax import numpy as jp
+import numpy as np
+from flax.training import orbax_utils
+from flax import struct
+from matplotlib import pyplot as plt
+import mediapy as media
+from orbax import checkpoint as ocp
+
+import mujoco
+from mujoco import mjx
+
+from brax import base
+from brax import envs
+from brax import math
+from brax.base import Base, Motion, Transform
+from brax.envs.base import Env, PipelineEnv, State
+from brax.mjx.base import State as MjxState
+from brax.training.agents.ppo import train as ppo
+from brax.training.agents.ppo import networks as ppo_networks
+from brax.io import html, mjcf, model
+
+mj_model = mujoco.MjModel.from_xml_path('./testing/assets/scene2.xml')
+mj_data = mujoco.MjData(mj_model)
+renderer = mujoco.Renderer(mj_model)
+
+mjx_model = mjx.put_model(mj_model)
+mjx_data = mjx.put_data(mj_model, mj_data)
