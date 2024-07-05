6b42c78acc54126a45565d7f1cc6d6e87162c443
Telios <victor.mittermair@gmail.com>
2024-07-05 06:34:38 +0000

    fix env file, brax tests

--- testing/brax_test.py
+++ testing/brax_test.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, Sequence, Tuple, Union
 import os
 os.environ['MUJOCO_GL'] = 'egl'
 from ml_collections import config_dict
+import cv2 as cv
 
 import jax
 from jax import numpy as jp
@@ -13,7 +14,6 @@ import numpy as np
 from flax.training import orbax_utils
 from flax import struct
 from matplotlib import pyplot as plt
-import mediapy as media
 from orbax import checkpoint as ocp
 
 import mujoco
@@ -29,9 +29,40 @@ from brax.training.agents.ppo import train as ppo
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.io import html, mjcf, model
 
-mj_model = mujoco.MjModel.from_xml_path('./testing/assets/scene2.xml')
+xml = """
+<mujoco>
+  <worldbody>
+    <light name="top" pos="0 0 1"/>
+    <body name="box_and_sphere" euler="0 0 -30">
+      <joint name="swing" type="hinge" axis="1 -1 0" pos="-.2 -.2 -.2"/>
+      <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+      <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+mj_model = mujoco.MjModel.from_xml_path('./assets/scene2.xml')
 mj_data = mujoco.MjData(mj_model)
 renderer = mujoco.Renderer(mj_model)
 
 mjx_model = mjx.put_model(mj_model)
 mjx_data = mjx.put_data(mj_model, mj_data)
+
+print(mj_data.qpos, type(mj_data.qpos))
+print(mjx_data.qpos, type(mjx_data.qpos), mjx_data.qpos.devices())
+
+jit_step = jax.jit(mjx.step)
+
+duration = 20
+frames = []
+framerate = 30
+
+mujoco.mj_resetData(mj_model, mj_data)
+while mjx_data.time < duration:
+    mjx_data = jit_step(mjx_model, mjx_data)
+    if len(frames) < mjx_data.time * framerate:
+        mj_data = mjx.get_data(mj_model, mjx_data)
+        renderer.update_scene(mj_data)
+        pixels = renderer.render()
+        frames.append(pixels)
